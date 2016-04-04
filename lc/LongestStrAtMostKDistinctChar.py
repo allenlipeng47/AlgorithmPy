96d98cd05e34bc3e38c0590c962b3a34bb96e37e
allenlipeng47 <allen.lipeng47@gmail.com>
2016-04-04 01:39:54 -0700

    Fixed bug

--- lc/LongestStrAtMostKDistinctChar.py
+++ lc/LongestStrAtMostKDistinctChar.py
@@ -1,18 +1,20 @@
 # http://cherylintcode.blogspot.com/2015/07/longest-substring-with-at-most-k.html
 # Solved by hashmap. a practice for python and algorithm
 
+
 class Solution(object):
+
     def longestStrKDistinct(self, str, k):
         left, num, ans, hm = 0, 0, 0, {}
         for right in range(len(str)):
             ch = str[right]
-            if (ch not in hm or hm[ch] == 0):
+            if ch not in hm or hm[ch] == 0:
                 num += 1
-                hm[ch] = hm.get(ch, 0) + 1
+            hm[ch] = hm.get(ch, 0) + 1
             while num > k:
                 ch = str[left]
                 hm[ch] -= 1
-                if (hm[ch] <= 0):
+                if hm[ch] <= 0:
                     num -= 1
                 left += 1
             ans = max(ans, right - left + 1)
