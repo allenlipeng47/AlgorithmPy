d98e97aa9855d120318a4bb58ba9ae81615b9d6c
allenlipeng47 <allen.lipeng47@gmail.com>
2016-04-04 00:41:23 -0700

    http://cherylintcode.blogspot.com/2015/07/longest-substring-with-at-most-k.html Use hashmap to solve.

---
+++ Test/LongestStrAtMostKDistinctCharTest.py
@@ -0,0 +1,15 @@
+import unittest
+from lc.LongestStrAtMostKDistinctChar import Solution
+
+class SolutionTest(unittest.TestCase):
+
+    def test(self):
+        s = Solution()
+        assert s.longestStrKDistinct('eceba', 2) == 3
+        assert s.longestStrKDistinct('eceba', 3) == 4
+        assert s.longestStrKDistinct('asdfsag', 3) == 4
+        assert s.longestStrKDistinct('asdfsag', 4) == 6
+        assert s.longestStrKDistinct('asdfsag', 0) == 0
+        assert s.longestStrKDistinct('asdfsag', 1) == 1
+        assert s.longestStrKDistinct('asdfsag', 7) == 7
+        assert s.longestStrKDistinct('asdfsag', 8) == 7
